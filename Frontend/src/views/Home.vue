57c8f6aa19e629f1ce489d2a795ad1f63cdea24b
PengshLiu <pengshuoliu@gmail.com>
2021-07-21 09:33:25 -0400

    frontend with backend

---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+        <DirectLabour/>
+    </div>
+</template>
+
+<script>
+import DirectLabour from '@/components/DirectLabour.vue'
+export default {
+    name: 'Home',
+    components:{
+        DirectLabour
+    }
+}
+</script>
